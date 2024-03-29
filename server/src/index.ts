d77e22796f5facc6001af322b4ac783f9467617a
Isaac <yisehakmitiku@gmail.com>
2024-03-29 07:00:59 -0700

    implement api endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,85 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import express from "express";
 
-// const express = require("express");
-// const prisma
 const PORT = 8080;
 const app = express();
 const prisma = new PrismaClient();
+
+app.use(express.json());
+
+// Get products
+app.get("/products", async (req, res) => {
+  const products = prisma.product.findMany();
+  res.json(products);
+});
+
+// Add a product
+app.post("/add-product", async (req, res) => {
+  const { name, description, status, price, tags, ownerId } = req.body;
+  const result = await prisma.product.create({
+    data: { name, description, status, price, tags, ownerId },
+  });
+  res.json(result);
+});
+
+// Update a product
+app.put("/update-product/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, description, status, price, tags } = req.body;
+  try {
+    const product = await prisma.product.update({
+      where: { id: id },
+      data: { name, description, status, price, tags },
+    });
+
+    res.json(product);
+  } catch (error) {
+    res.json({
+      error: `Post with ID ${id} does not exist in the database`,
+    });
+  }
+});
+
+// Delete a product
+app.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = prisma.product.delete({
+    where: { id: id },
+  });
+  res.json(product);
+});
+
+// Search products by name or tag
+app.get("/search-product", async (req, res) => {
+  const { query } = req.query;
+
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: query as string,
+              mode: "insensitive", // Case insensitive search
+            },
+          },
+          {
+            tags: {
+              contains: query as string,
+              mode: "insensitive", // Case insensitive search
+            },
+          },
+        ],
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+    });
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to search products." });
+  }
+});
 
 // Define a route handler for the root endpoint
 app.get("/", (req, res) => {
